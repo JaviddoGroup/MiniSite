124f1c305359b25c43480ce944733cccd0feaf28
JaviddoGroup <154234110+JaviddoGroup@users.noreply.github.com>
2024-03-25 17:22:40 +0400

    ResPonsive Edit

---
+++ pages/Certificates/js/script.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var menuIcon = document.querySelector('.menu-icon');
+    var menus = document.querySelector('.menus');
+
+    menuIcon.addEventListener('click', function () {
+        menus.classList.toggle('menu-active');
+    });
+
+    // Получаем все ссылки меню
+    var menuLinks = document.querySelectorAll('.menus a');
+
+    // Обработчик события прокрутки
+    window.addEventListener('scroll', function () {
+        // Перебираем все ссылки меню
+        menuLinks.forEach(function (link) {
+            // Получаем элемент, к которому ссылка ведет
+            var targetElement = document.querySelector(link.getAttribute('href'));
+            // Проверяем, находится ли элемент в области видимости
+            if (targetElement.getBoundingClientRect().top < window.innerHeight * 0.3 && targetElement.getBoundingClientRect().bottom > window.innerHeight * 0) {
+                // Если да, добавляем класс active
+                link.classList.add('nav-bar-active');
+            } else {
+                // Если нет, удаляем класс active
+                link.classList.remove('nav-bar-active');
+            }
+        });
+    });
+});
+window.addEventListener('load', function () {
+    var loader = document.querySelector('.spinnerContainer');
+    loader.style.display = 'none'; // Скрыть элемент загрузки после загрузки страницы
+});
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const languageSpans = document.querySelectorAll('.name');
+
+    languageSpans.forEach(function (span) {
+        span.addEventListener('click', function () {
+            const lang = this.textContent.toLowerCase();
+            fetch(`./languages/${lang}.json`)
+                .then(response => response.json())
+                .then(data => {
+                    replaceKeywords(data);
+                })
+                .catch(error => console.error('Error:', error));
+        });
+    });
+
+    function replaceKeywords(data) {
+        const elementsWithKeywords = document.querySelectorAll('[data-keyword]');
+        elementsWithKeywords.forEach(function (element) {
+            const keyword = element.getAttribute('data-keyword');
+            if (data.hasOwnProperty(keyword)) {
+                element.textContent = data[keyword];
+            }
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.addEventListener('keydown', function (event) {
+    if (event.keyCode === 123) { // F12
+        console.log('Консоль открыта');
+        // Здесь можно предпринять дополнительные действия
+    }
+});
